9c3d6aaddadf7a4611f657805d2e2210c2cd458d
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-05-01 21:12:51 +0200

    feat: add -v --svelte param

--- lib/svelte_definition.sh
+++ lib/svelte_definition.sh
@@ -5,7 +5,9 @@ parser_definition_svelte() {
     msg -- 'getoptions basic example' ''
     msg -- 'Options:'
     flag FLAG_P -p +p --{no-}flag-p on:0 no:1 init:@no -- "Use -p not to run pnpm update"
+    flag FLAG_S -s +s --{no-}flag-s on:0 no:1 init:@no -- "Use -s not to run pnpm i -D svelte@next"
     flag FLAG_T -t +t --{no-}flag-t on:0 no:1 init:@no -- "Use -t not to run pnpm test:integration"
     flag FLAG_G -g +g --{no-}flag-t on:0 no:1 init:@no -- "Use -g not to run git add, commit, and push"
+    param SVELTE_NEXT -v --svelte validate:number -- "Svelte 5 version number"
     disp :usage -h --help
 }
--- src/update.sh
+++ src/update.sh
@@ -3,26 +3,24 @@ fn_update() {
   # Get the desired Svelte version from the script argument
   bannerColor 'Welcome to svelte update.' "blue" "*"
 
-  svelte_version="$1"
-
-  # Check if an argument is provided
-  if [ -z "$svelte_version" ]; then
-    bannerColor '"Error: Please provide the Svelte version (e.g., updateSvelte i 120)' "red" "*"
+  if [[ $# -gt 0  ]];then
+    target_dir="$1"
+  else
+    bannerColor 'Error: Please provide the target directory.' "red" "*"
     exit 1
+  fi
+
+  svelte_version="next"
+
+  if [[ "$SVELTE_NEXT" ]];then
+    svelte_version="$SVELTE_NEXT"
   fi
 
   # Check for a minimum of two arguments (version and target directory)
-  if [ $# -lt 2 ]; then
-    bannerColor '"Error: Please provide the Svelte version and target directory (e.g., updateSvelte i 120 ./my-project).' "red" "*"
-    exit 1
-  fi
-
-  # Get the desired Svelte version (shift argument list to remove the first argument)
-  svelte_version="$1"
-  shift
-
-  # Get the target directory (remaining arguments after shift)
-  target_dir="$*"
+  # if [ $# -lt 2 ]; then
+  #   bannerColor '"Error: Please provide the Svelte version and target directory (e.g., updateSvelte i 120 ./my-project).' "red" "*"
+  #   exit 1
+  # fi
 
   # Check if the target directory exists and is a directory
   if ! [ -d "$target_dir" ]; then
@@ -30,59 +28,63 @@ fn_update() {
     exit 1
   fi
 
-  bannerColor "This script will update svelte version, run pnpm update, git add, commit, and push, pnpm test:integration in $target_dir to $svelte_version. Use -h or --help for help." "blue" "*"
+  bannerColor "This script will update svelte version 5 $svelte_version in the $target_dir directories." "blue" "*"
 
+  bannerColor "And run pnpm update, git add, commit, push, pnpm test:integration." "blue" "*"
+  
+  bannerColor "Use -h or --help for help. " "blue" "*"
 
   for directory in "$target_dir"/* ; do
-    bannerColor "Checking $directory" "blue" "*"
-
-    if [ -d "$directory" -a -d "$directory/.git" ]; then
+    if [ -d "$directory" ]; then 
       cd "$directory"
+      bannerColor "Checking $directory" "blue" "*"
 
-      # Get current Svelte version
-      current_version=$(pnpm list svelte --depth=0 | awk '{print $2}')
-      update_needed=false
+      if [ -d "$directory/.git" ]; then
+        # Get current Svelte version
+        current_version=$(pnpm list svelte --depth=0 | awk '{print $2}')
+        update_needed=false
 
-      if [[ "$current_version" =~ "next" ]]; then
-        # If current_version has "next", check version comparison
-        if [[ "$(semver compare "$svelte_version" "$current_version")" -lt "1" ]]; then
-          update_needed=true 
+        if [[ "$current_version" =~ "next" ]]; then
+          # If current_version has "next", check version comparison
+          if [[ "$(semver compare "$svelte_version" "$current_version")" -lt "1" ]]; then
+            update_needed=true 
+          fi
         fi
-      fi
 
-      if [[ $update_needed == true ]]; then
+        if [[ $update_needed == true ]]; then
 
-        if [[ $FLAG_P == 1 ]];then
-          bannerColor "Running pnpm update in $directory ..." "magenta" "*"
-          pnpm update
+          if [[ $FLAG_P == 1 ]];then
+            bannerColor "Running pnpm update in $directory ..." "magenta" "*"
+            pnpm update
+            bannerColor "Update completed" "green" "*"
+          fi
+          
+          
+          bannerColor "Running pnpm i -D svelte@$svelte_version ..." "magenta" "*"
+          pnpm i -D svelte@"5.0.0-next.$svelte_version"
           bannerColor "Update completed" "green" "*"
+          
+          if [[ $FLAG_T == 1 ]];then
+            bannerColor "Running pnpm test:integration ..." "magenta" "*"
+            pnpm test:integration
+            bannerColor "Test completed" "green" "*"
+          fi
+    
+          if [[ $FLAG_G == 1 ]];then
+            bannerColor "Running git commands ..." "magenta" "*"
+            git add -A && git commit --message "Update Svelte to $svelte_version" && git push
+            bannerColor "Git commands completed" "green" "*"
+          fi
+          
+        else
+          bannerColor  "Svelte version $current_version (next) is likely up-to-date (not updating)." "blue" "*"
         fi
-        
-        
-        bannerColor "Running pnpm i -D svelte@$svelte_version ..." "magenta" "*"
-        pnpm i -D svelte@"5.0.0-next.$svelte_version"
-        bannerColor "Update completed" "green" "*"
-        
-        if [[ $FLAG_T == 1 ]];then
-          bannerColor "Running pnpm test:integration ..." "magenta" "*"
-          pnpm test:integration
-          bannerColor "Test completed" "green" "*"
-        fi
-  
-        if [[ $FLAG_G == 1 ]];then
-          bannerColor "Running git commands ..." "magenta" "*"
-          git add -A && git commit --message "Update Svelte to $svelte_version" && git push
-          bannerColor "Git commands completed" "green" "*"
-        fi
-        
+        cd ..
       else
-        bannerColor  "Svelte version $current_version (next) is likely up-to-date (not updating)." "blue" "*"
+        bannerColor "Directory $directory does not exist." "red" "*"
       fi
-      cd ..
-    elif [ -d "$directory" ]; then
-      bannerColor "Directory $directory exists, but it lacks a .git directory." "red" "*"
     else
-      bannerColor "Directory $directory does not exist." "red" "*"
+      bannerColor "Directory $directory is not a directory." "red" "*"
     fi
   done
 }
